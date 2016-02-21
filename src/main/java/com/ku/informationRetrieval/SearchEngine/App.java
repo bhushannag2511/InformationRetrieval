c849c5ba3211e0d1065fd8d2ca15e11295c59750
Bhushan <bhushan2511@gmail.com>
2016-02-20 23:34:23 -0600

    First

--- src/main/java/com/ku/informationRetrieval/SearchEngine/App.java
+++ src/main/java/com/ku/informationRetrieval/SearchEngine/App.java
@@ -8,10 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Hello World!11111" );
-=======
         System.out.println( "Hello World2222!" );
->>>>>>> master
     }
 }
