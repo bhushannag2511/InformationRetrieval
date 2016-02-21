c27580f599be1613132a50b6ce8e7afd0961126e
Bhushan <bhushan2511@gmail.com>
2016-02-20 22:25:01 -0600

    Initial Project Structure Creation by Bhushan

---
+++ src/main/java/com/ku/informationRetrieval/SearchEngine/App.java
@@ -0,0 +1,13 @@
+package com.ku.informationRetrieval.SearchEngine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
